2d77ca25fa9828506333441bb3d14e2da75d11c2
Bleon12345 <72931239+Bleon12345@users.noreply.github.com>
2022-05-12 19:33:54 +0200

    memet du lelek die deine Datei
    qitma nje qaje

---
+++ teilbar-12-5-2022.py
@@ -0,0 +1,37 @@
+# Einleitung: wofür wird das verwendet?:
+
+# Eine Funktion erstellen die ermittelt ob eine
+# Zahl gerade oder ungerade ist
+# 101 / 2
+# print(101 % 2)
+# print(101 % 2)  # modulo Operator
+
+
+#def gerade_oder_ungerade():
+#     while True:
+#        zahl_str = input()
+#        zahl_int = int(zahl_str)
+#        if zahl_int % 2 == 0:
+#           print("gerade")
+#        else:
+#            print("ungerade")
+
+#gerade_oder_ungerade()
+
+# Eine Funktion erstellen die ermittelt ob eine
+# Zahl durch drei teilbar ist oder nicht
+
+def Drei():
+    while True:
+        zahl_str = input()
+        zahl_int = int(zahl_str)
+        if zahl_int % 3 == 0:
+            print("Durch Drei teilbar")
+        else:
+            print("bist du eigentlich geistig zurückgeblieben dass du so einen Text geschrieben hast nur um am Ende was falsches aufzuschreiben")
+
+Drei()
+
+
+# Eine Funktion erstellen die ermittelt ob eine
+# Zahl durch neun teilbar ist oder nicht
